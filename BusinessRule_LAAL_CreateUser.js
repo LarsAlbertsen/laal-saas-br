4062ab8a3fcae8939a801390630042e7ec26cd3d
Lars Albertsen <laal@stibosystems.com>
2023-01-25 11:26:33 -0500

    Committed from STEP 2023-01-25T16:26Z

---
+++ BusinessRule_LAAL_CreateUser.js
@@ -0,0 +1,40 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LAAL_CreateUser",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "LAAL_CreateUser",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+var stiboGroup = manager.getGroupHome().getGroupByID("Stibo");
+var newUser = stiboGroup.createUser("NewUser2", "Hello","laal@stibosystems.com");
+newUser.setName("My New User");
+}
---
+++ BusinessRule_SystemChangeNOP.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "SystemChangeNOP",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "System Change NOP",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
