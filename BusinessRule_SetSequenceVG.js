9e3f1caa45f6590a8b6a24cc0f3ad39732d5ef84
Lars Albertsen <laal@stibosystems.com>
2023-01-25 11:25:24 -0500

    Committed from STEP 2023-01-25T16:25Z

---
+++ BusinessRule_SetSequenceVG.js
@@ -0,0 +1,44 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "SetSequenceVG",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "SetSequenceVG",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Trigger",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "AttributeComparatorCondition",
+  "parameters" : [ {
+    "id" : "Attribute1",
+    "type" : "com.stibo.core.domain.Attribute",
+    "value" : "ValueFromVG"
+  }, {
+    "id" : "Attribute2",
+    "type" : "com.stibo.core.domain.Attribute",
+    "value" : null
+  }, {
+    "id" : "Constant",
+    "type" : "java.lang.String",
+    "value" : ""
+  }, {
+    "id" : "Operator",
+    "type" : "java.lang.String",
+    "value" : "="
+  } ],
+  "pluginType" : "Precondition"
+}
+*/
