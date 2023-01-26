639d52f139cc883e94a5fc27b174fc975d7db5f2
Lars Albertsen <laal@stibosystems.com>
2023-01-26 16:12:08 -0500

    Committed from STEP 2023-01-26T21:12Z

--- BusinessRule_LAAL_CreateUser.js
+++ BusinessRule_LAAL_CreateUser.js
@@ -34,7 +34,9 @@
 }
 */
 exports.operation0 = function (manager) {
+//
 // Create a new user in system
+//
 var stiboGroup = manager.getGroupHome().getGroupByID("Stibo");
 var newUser = stiboGroup.createUser("NewUser2", "Hello","laal@stibosystems.com");
 newUser.setName("My New User");
