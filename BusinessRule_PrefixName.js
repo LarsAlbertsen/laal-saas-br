59fd6f306eabb52693a9a858d32a0cbcd10b933c
Lars Albertsen <laal@stibosystems.com>
2023-01-31 10:52:11 -0500

    Committed from STEP 2023-01-31T15:52Z

--- BusinessRule_PrefixName.js
+++ BusinessRule_PrefixName.js
@@ -12,10 +12,34 @@
   "name" : "PrefixName",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "Item" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+var curName = node.getName();
+if (curName==null) {
+	curName = "";
+}
+var newName = "Prefix "+curName;
+node.setName(newName);
+
+
+}
