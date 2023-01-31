bf67f759beb7184a68634744afff310f090b46e6
Lars Albertsen <laal@stibosystems.com>
2023-01-31 11:44:08 -0500

    Committed from STEP 2023-01-31T16:44Z

--- BusinessRule_PrefixNameOnSelected.js
+++ BusinessRule_PrefixNameOnSelected.js
@@ -13,7 +13,7 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ "Item" ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
@@ -23,8 +23,8 @@
 {
   "pluginId" : "JavaScriptBusinessActionWithBinds",
   "binds" : [ {
-    "contract" : "CurrentObjectBindContract",
-    "alias" : "node",
+    "contract" : "WebUiContextBind",
+    "alias" : "web",
     "parameterClass" : "null",
     "value" : null,
     "description" : null
@@ -33,13 +33,20 @@
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (node) {
-var curName = node.getName();
-if (curName==null) {
-	curName = "";
+exports.operation0 = function (web) {
+var selection = web.getSelected();
+for (var i=0; i<selection.size(); i++) {
+	var node = selection.get(i);
+
+	var curName = node.getName();
+	if (curName==null) {
+		curName = "";
+	}
+	var newName = "Prefix "+curName;
+	node.setName(newName);
+
 }
-var newName = "Prefix "+curName;
-node.setName(newName);
+
 
 
 }
