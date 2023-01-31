acbbb6e8878e248555b7d03d634caee6dfde3ec7
Lars Albertsen <laal@stibosystems.com>
2023-01-31 10:51:10 -0500

    Committed from STEP 2023-01-31T15:51Z

---
+++ BusinessRule_PrefixName.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "PrefixName",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "PrefixName",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
