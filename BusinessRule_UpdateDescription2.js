790d22b680f7849104623be85d9d0af0bc165763
Lars Albertsen <laal@stibosystems.com>
2023-02-03 07:50:12 -0500

    Committed from STEP 2023-02-03T12:50Z

--- BusinessRule_UpdateDescription2.js
+++ BusinessRule_UpdateDescription2.js
@@ -13,9 +13,41 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "BusinessConditionBindContract",
+    "alias" : "isActive",
+    "parameterClass" : "com.stibo.core.domain.impl.businessrule.FrontBusinessConditionImpl",
+    "value" : "isActive",
+    "description" : null
+  }, {
+    "contract" : "BusinessActionBindContract",
+    "alias" : "updateDescription",
+    "parameterClass" : "com.stibo.core.domain.impl.businessrule.FrontBusinessActionImpl",
+    "value" : "UpdateDescriptionIfNotActive",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,isActive,updateDescription) {
+if (isActive.evaluate(node)) {
+	updateDescription.execute(node)
+}
+
+}
