36f983f6816ccd5f76025666b3b890cd2906ecdb
Lars Albertsen <laal@stibosystems.com>
2023-02-03 07:38:08 -0500

    Committed from STEP 2023-02-03T12:38Z

--- BusinessRule_isActive.js
+++ BusinessRule_isActive.js
@@ -19,3 +19,20 @@
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessConditionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+return true
+}
