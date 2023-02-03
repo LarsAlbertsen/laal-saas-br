04b5004acd5058945a07d6f3182fc9407c2292bd
Lars Albertsen <laal@stibosystems.com>
2023-02-03 11:23:11 -0500

    Committed from STEP 2023-02-03T16:23Z

---
+++ BusinessRule_ELFREVPBr.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ELFREVPBr",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "ELFREVPBr",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
