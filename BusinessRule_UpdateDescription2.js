bfb9b85b75600af368353100f61dc2b80f069898
Lars Albertsen <laal@stibosystems.com>
2023-02-03 07:47:11 -0500

    Committed from STEP 2023-02-03T12:47Z

---
+++ BusinessRule_UpdateDescription2.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "UpdateDescription2",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "UpdateDescription2",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
--- BusinessRule_UpdateDescriptionIfNotActive.js
+++ BusinessRule_UpdateDescriptionIfNotActive.js
@@ -13,9 +13,39 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "BusinessConditionBindContract",
+    "alias" : "isActive",
+    "parameterClass" : "com.stibo.core.domain.impl.businessrule.FrontBusinessConditionImpl",
+    "value" : "isActive",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,isActive) {
+if (isActive.evaluate(node)) {
+	.... handle active node
+} else {
+	.... handle inactive node
+}
+
+
+
+}
