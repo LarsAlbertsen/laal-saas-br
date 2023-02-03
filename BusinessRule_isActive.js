abbbdaf8a484cd5115713688a7e05ab7b1b4d608
Lars Albertsen <laal@stibosystems.com>
2023-02-03 07:37:08 -0500

    Committed from STEP 2023-02-03T12:37Z

---
+++ BusinessRule_isActive.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "isActive",
+  "type" : "BusinessCondition",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "isActive",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
