54172576f24110d51905d7882ad41c12a4303881
Lars Albertsen <laal@stibosystems.com>
2023-02-03 15:33:14 -0500

    Committed from STEP 2023-02-03T20:33Z

---
+++ BusinessRule_CreateFamilyFromSelectedItems.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "CreateFamilyFromSelectedItems",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "CreateFamilyFromSelectedItems",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "WebUiContextBind",
+    "alias" : "web",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (web) {
+var selection = web.getSelection();
+logger.info("selection="+selection);
+}
