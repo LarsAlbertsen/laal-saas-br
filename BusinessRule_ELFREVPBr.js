1ceb2e88008d887ad832f571b2529de15e1ccbaf
Lars Albertsen <laal@stibosystems.com>
2023-02-03 11:24:11 -0500

    Committed from STEP 2023-02-03T16:24Z

--- BusinessRule_ELFREVPBr.js
+++ BusinessRule_ELFREVPBr.js
@@ -13,9 +13,26 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+logger.info('ELFREVP ' + node.getID())
+}
