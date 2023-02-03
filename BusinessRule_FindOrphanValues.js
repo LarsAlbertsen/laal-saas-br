c4ee24064f466b8de40da8ddfe59048b518e06c8
Lars Albertsen <laal@stibosystems.com>
2023-02-03 15:20:11 -0500

    Committed from STEP 2023-02-03T20:20Z

--- BusinessRule_FindOrphanValues.js
+++ BusinessRule_FindOrphanValues.js
@@ -12,10 +12,34 @@
   "name" : "FindOrphanValues",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "Branch", "Family", "Item", "Leaf", "Variant" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+var allValues = node.getValues().toArray();
+for (var i=0; i<allValues.length; i++) {
+	var aValue = allValues[i];
+	if (aValue.isOrphan()) {
+		logger.info(node.getID()+"\t"+aValue.getAttribute().getTitle()+"="+aValue.getSimpleValue());
+	}
+}
+
+}
