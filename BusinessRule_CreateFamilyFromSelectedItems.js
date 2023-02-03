ef83729d98c38ff78728461e1019aff01c875960
Lars Albertsen <laal@stibosystems.com>
2023-02-03 16:02:10 -0500

    Committed from STEP 2023-02-03T21:02Z

--- BusinessRule_CreateFamilyFromSelectedItems.js
+++ BusinessRule_CreateFamilyFromSelectedItems.js
@@ -66,17 +66,19 @@ for (var i=0; i<selection.size(); i++) {
 			for (var j=0; j<nameParts.length; j++) {
 				familyName.add(nameParts[j].trim());
 			}
+			logger.info("Initial familyName="+familyName);
 		}
 		else {
-			logger.info("checking familyName="+familyName);
+			logger.info("checking familyName="+familyName +" with "+n.getName());
 
 			var l = new java.util.ArrayList();
 			for (var j=0; j<familyName.length; j++) {
-				var ckeckName = familyName.get(j);
-				
+				var checkName = familyName.get(j);
+
+				var nameParts = n.getName().split(" ");
 				for (var j=0; j<nameParts.length; j++) {
 					var part = nameParts[j].trim();
-					logger.info("Checking "+ckeckName+" "+ part);
+					logger.info("Checking "+ part);
 					if (checkName.equalsIgnoreCase(part)) {
 						logger.info("Keeping "+part);
 						l.add(part);
