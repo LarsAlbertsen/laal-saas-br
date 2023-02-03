791ecf1dc4d1e8be78d1be8ef66346e12a4fb258
Lars Albertsen <laal@stibosystems.com>
2023-02-03 07:39:09 -0500

    Committed from STEP 2023-02-03T12:39Z

---
+++ BusinessRule_UpdateDescriptionIfNotActive.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "UpdateDescriptionIfNotActive",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "Update Description",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
