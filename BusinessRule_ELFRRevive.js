c8024d5e86afa88497c5ccf8f4252a387da14feb
Lars Albertsen <laal@stibosystems.com>
2023-02-07 10:57:12 -0500

    Committed from STEP 2023-02-07T15:57Z

--- BusinessRule_ELFRRevive.js
+++ BusinessRule_ELFRRevive.js
@@ -13,9 +13,33 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (logger,manager) {
+var nodeHome = manager.getNodeHome()
+manager.getPRoductHome().getProductByID('107239')
+}
