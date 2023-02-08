ce1263d202e82aa83f577c2684d8fae8d4f7d784
Lars Albertsen <laal@stibosystems.com>
2023-02-08 09:41:12 -0500

    Committed from STEP 2023-02-08T14:41Z

---
+++ BusinessRule_ELFRLog.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ELFRLog",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "ELFRLog",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
