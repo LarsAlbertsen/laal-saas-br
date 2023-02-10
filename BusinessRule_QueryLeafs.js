07ac9674843a0b3666e8107dee72482b36a07404
Lars Albertsen <laal@stibosystems.com>
2023-02-10 11:10:13 -0500

    Committed from STEP 2023-02-10T16:10Z

---
+++ BusinessRule_QueryLeafs.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "QueryLeafs",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "QueryLeafs",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
