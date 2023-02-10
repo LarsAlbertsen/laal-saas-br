56f9bb5776b9e765a8cdc596f103cd7113771429
Lars Albertsen <laal@stibosystems.com>
2023-02-10 11:42:10 -0500

    Committed from STEP 2023-02-10T16:42Z

---
+++ BusinessRule_QueryItem.js
@@ -0,0 +1,85 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "QueryItem",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "QueryItem",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessFunctionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "log",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ObjectTypeBindContract",
+    "alias" : "ItemObjType",
+    "parameterClass" : "com.stibo.core.domain.impl.ObjectTypeImpl",
+    "value" : "Item",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation",
+  "functionReturnType" : "com.stibo.query.home.QuerySpecification",
+  "functionParameterBinds" : [ {
+    "contract" : "NodeBindContract",
+    "alias" : "currentNode",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : ""
+  }, {
+    "contract" : "StringBindContract",
+    "alias" : "searchString",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : ""
+  } ]
+}
+*/
+exports.operation0 = function (log,manager,ItemObjType,currentNode,searchString) {
+var c = com.stibo.query.condition.Conditions;
+var h = manager.getHome(com.stibo.query.home.QueryHome);
+var querySpecification = h.queryFor(com.stibo.core.domain.Product).where(
+	c.name().like(searchString)
+     .and(c.objectType(ItemObjType))
+);
+
+/*
+
+var querySpecification = h.queryFor(com.stibo.core.domain.entity.Entity).where(
+     c.valueOf(active).eq("No")
+     .and(c.objectType(location))
+ );
+
+var querySpecification = h.queryFor(com.stibo.core.domain.Product).where(
+     c.objectType(LeafObjType)
+);
+
+  
+  */
+ 
+return querySpecification;
+}
