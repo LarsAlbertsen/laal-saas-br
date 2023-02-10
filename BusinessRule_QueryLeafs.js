a39f3a6db1f74d62c076c9e2483fbc4b0e0ab4fd
Lars Albertsen <laal@stibosystems.com>
2023-02-10 11:14:14 -0500

    Committed from STEP 2023-02-10T16:14Z

--- BusinessRule_QueryLeafs.js
+++ BusinessRule_QueryLeafs.js
@@ -19,3 +19,53 @@
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessFunctionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "log",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ObjectTypeBindContract",
+    "alias" : "LeafObjType",
+    "parameterClass" : "com.stibo.core.domain.impl.ObjectTypeImpl",
+    "value" : "Leaf",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation",
+  "functionReturnType" : "com.stibo.query.home.QuerySpecification",
+  "functionParameterBinds" : [ {
+    "contract" : "NodeBindContract",
+    "alias" : "currentNode",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : ""
+  }, {
+    "contract" : "StringBindContract",
+    "alias" : "searchString",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : ""
+  } ]
+}
+*/
+exports.operation0 = function (log,manager,LeafObjType,currentNode,searchString) {
+var c = com.stibo.query.condition.Conditions;
+ var h = manager.getHome(com.stibo.query.home.QueryHome);
+ var querySpecification = h.queryFor(com.stibo.core.domain.Product).where(
+     c.objectType(LeafObjType)
+);
+
+ 
+return querySpecification;
+}
