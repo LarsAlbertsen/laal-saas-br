4c0a985eabefdab0cbba357364fb18938b585710
Lars Albertsen <laal@stibosystems.com>
2023-02-14 10:02:12 -0500

    Committed from STEP 2023-02-14T15:02Z

---
+++ BusinessRule_ELFRTest.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ELFRTest",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "ELFRTest",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
