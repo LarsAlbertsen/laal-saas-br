d6559dfb30611c59f9b3a3341ca16b97323628d0
Lars Albertsen <laal@stibosystems.com>
2023-02-16 06:09:14 -0500

    Committed from STEP 2023-02-16T11:09Z

--- BusinessRule_CreateFamilyFromSelectedItems.js
+++ BusinessRule_CreateFamilyFromSelectedItems.js
@@ -56,49 +56,39 @@ var selection = web.getSelection();
 logger.info("node="+node.getTitle());
 logger.info("selection="+selection);
 
-var familyName = new java.util.ArrayList();
+var candidates = new java.util.ArrayList();
 for (var i=0; i<selection.size(); i++) {
-	var n = selection.get(i);
-	logger.info("currentChild ["+n.getName()+"]");
-	if (n.getName()!=null) {
+	var currentNode = selection.get(i);
+	logger.info("currentNode ["+currentNode.getName()+"]");
+	if (currentNode.getName()!=null) {
 		var nameParts = n.getName().split(" ");
-		if (familyName.size()==0) {
+		if (i==0) {
 			for (var j=0; j<nameParts.length; j++) {
-				familyName.add(nameParts[j].trim());
+				candidates.add(nameParts[j].trim());
 			}
-			logger.info("Initial familyName="+familyName);
 		}
 		else {
-			logger.info("before checking familyName="+familyName +" with "+n.getName());
-
-			var l = new java.util.ArrayList();
-			for (var j=0; j<familyName.size(); j++) {
-				var checkName = familyName.get(j);
-				//var nameParts = n.getName().split(" ");
-				logger.info("checkName "+j+" "+checkName+" in "+n.getName() +" partsSize="+nameParts.length);
-
-				/*for (var j=0; j<nameParts.length; j++) {
-					var part = nameParts[j].trim();
-					logger.info("Compare "+ j+" "+checkName+"="+part);
-					if (j
-					l.add(part);
-				}*/
-				/*
+			logger.info("candidates="+candidates);
+			/*if (familyName.size()==0) {
 				for (var j=0; j<nameParts.length; j++) {
-					var part = nameParts[j].trim();
-					logger.info("Checking "+ part);
-					if (checkName.equalsIgnoreCase(part)) {
-						logger.info("Keeping "+part);
-						l.add(part);
-					}
-					else {
-						logger.info("Removing "+part);
-					}
-				}*/
+					familyName.add(nameParts[j].trim());
+				}
+				logger.info("Initial familyName="+familyName);
 			}
-			familyName = l;
-			logger.info("after checking familyName="+familyName +" with "+n.getName());
-
+			else {
+				logger.info("before checking familyName="+familyName +" with "+n.getName());
+	
+				var l = new java.util.ArrayList();
+				for (var j=0; j<familyName.size(); j++) {
+					var checkName = familyName.get(j);
+					//var nameParts = n.getName().split(" ");
+					logger.info("checkName "+j+" "+checkName+" in "+n.getName() +" partsSize="+nameParts.length);
+	
+				}
+				familyName = l;
+				logger.info("after checking familyName="+familyName +" with "+n.getName());
+	
+			}*/
 		}
 	}
 }
