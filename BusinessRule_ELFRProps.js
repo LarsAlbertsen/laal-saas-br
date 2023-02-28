457a263d24d42a749634775d8bc2387a4070a738
Lars Albertsen <laal@stibosystems.com>
2023-02-28 15:20:12 -0500

    Committed from STEP 2023-02-28T20:20Z

--- BusinessRule_ELFRProps.js
+++ BusinessRule_ELFRProps.js
@@ -13,9 +13,37 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function () {
+var regs = ['InMemory.Plugin', 'TaskList.Streaming\..*', 'Portal.TaskList.Max']
+
+function matches(str) {
+	var found = false;
+	regs.every(function(regex) {
+		found = str.matches(regex)
+		return !found
+	})
+	return found
+}	
+
+var set = com.stibo.systemconfig.ConfigUtil.getAllProperties()
+set.toArray().forEach(function(k) {
+	//if ((k+'').match(/InMemory\.Plugin/)) 
+	if (matches(k))
+		logger.info('ConfigUtil: ' + k + ' = ' + com.stibo.systemconfig.ConfigUtil.getProperty(k))	
+})
+
+}
