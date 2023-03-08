1d89d8a2fb30ed87d6c2a89b44d36aff5bbdc046
Lars Albertsen <laal@stibosystems.com>
2023-03-08 13:50:13 -0500

    Committed from STEP 2023-03-08T18:50Z

--- BusinessRule_CreateGarbageData.js
+++ BusinessRule_CreateGarbageData.js
@@ -12,10 +12,46 @@
   "name" : "CreateGarbageData",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "Item" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ReferenceTypeBindContract",
+    "alias" : "refType",
+    "parameterClass" : "com.stibo.core.domain.impl.ReferenceTypeImpl",
+    "value" : "Accessory",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,refType) {
+var productCount = 10000;
+node.approve();
+
+for (var i=0; i<5; i++) {
+	var targetID = "LAAL-"+Math.floor(Math.random() * productCount);
+	logger.info("TargetID="+target);
+	var target = node.getManager().getProductHome().getProductByID(targetID);
+	if (target!=null) {
+		logger.info("Create ref");
+		node.createReference(target, refType);
+		node.approve();
+	}
+}
+
+}
