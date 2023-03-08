5e7f6a2ded2511e9fe73818b6c3669f5be4862db
Lars Albertsen <laal@stibosystems.com>
2023-03-08 13:52:14 -0500

    Committed from STEP 2023-03-08T18:52Z

--- BusinessRule_CreateGarbageData.js
+++ BusinessRule_CreateGarbageData.js
@@ -34,16 +34,23 @@
     "parameterClass" : "com.stibo.core.domain.impl.ReferenceTypeImpl",
     "value" : "Accessory",
     "description" : null
+  }, {
+    "contract" : "AttributeBindContract",
+    "alias" : "ItemAttr",
+    "parameterClass" : "com.stibo.core.domain.impl.AttributeImpl",
+    "value" : "ItemAttr",
+    "description" : null
   } ],
   "messages" : [ ],
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (node,refType) {
+exports.operation0 = function (node,refType,ItemAttr) {
 var productCount = 10000;
 node.approve();
 
 for (var i=0; i<5; i++) {
+	node.setSimpleValue(ItemAttr, ""+i);
 	var targetID = "LAAL-"+Math.floor(Math.random() * productCount);
 	logger.info("TargetID="+target);
 	var target = node.getManager().getProductHome().getProductByID(targetID);
