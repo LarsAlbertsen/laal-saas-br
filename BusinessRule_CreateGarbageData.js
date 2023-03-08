3790a89a71f25e11a7cf56bd6f44b00a2cb0ef34
Lars Albertsen <laal@stibosystems.com>
2023-03-08 13:45:14 -0500

    Committed from STEP 2023-03-08T18:45Z

---
+++ BusinessRule_CreateGarbageData.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "CreateGarbageData",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "CreateGarbageData",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
