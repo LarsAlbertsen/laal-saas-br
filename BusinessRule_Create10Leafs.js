1e8e1d40e6d9289d2ee0868ea087087625b18e44
Lars Albertsen <laal@stibosystems.com>
2023-03-16 08:13:16 -0400

    Committed from STEP 2023-03-16T12:13Z

--- BusinessRule_Create10Leafs.js
+++ BusinessRule_Create10Leafs.js
@@ -13,9 +13,30 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+
+for (var i=0; i<10; i++) {
+	node.createProduct("","Tree");
+}
+
+}
