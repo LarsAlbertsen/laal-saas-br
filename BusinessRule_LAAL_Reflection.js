4efc97655f623652894230e25ab6b052eb643d62
Lars Albertsen <laal@stibosystems.com>
2023-03-24 14:49:16 -0400

    Committed from STEP 2023-03-24T18:49Z

--- BusinessRule_LAAL_Reflection.js
+++ BusinessRule_LAAL_Reflection.js
@@ -12,10 +12,47 @@
   "name" : "LAAL_Reflection",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "Item" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+var allValues = node.getValues().toArray();
+for (var i=0; i<allValues.length; i++) {
+	var aValue = allValues[i];
+	var attribute = aValue.getAttribute();
+	var validatorName = getValidatorName(attribute);
+	if ("text".equals(validatorName) || "number".equals(validatorName) || "numeric_text".equals(validatorName)) {
+		logger.info("setTEXT");
+	}
+	else {
+		logger.warning("UNKNOWN "+validatorName);
+	}
+}
+
+
+function getValidatorName(pAttr) {
+	var method = pAttr.getClass().getMethod("getValidatorName");
+	//logger.info("Got Method");
+	var oo = method.invoke(pAttr);
+	return oo;
+}
+
+}
