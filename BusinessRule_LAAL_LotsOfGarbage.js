8e9f9db3c06477097c768ac2afb6c095b14f7bcd
Lars Albertsen <laal@stibosystems.com>
2023-03-28 16:21:14 -0400

    Committed from STEP 2023-03-28T20:21Z

---
+++ BusinessRule_LAAL_LotsOfGarbage.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LAAL_LotsOfGarbage",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "LAAL_LotsOfGarbage",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
