c62c803475b0444d2a2286ee58bfa8ab2d49e37a
Lars Albertsen <laal@stibosystems.com>
2023-03-28 16:22:15 -0400

    Committed from STEP 2023-03-28T20:22Z

--- BusinessRule_LAAL_LotsOfGarbage.js
+++ BusinessRule_LAAL_LotsOfGarbage.js
@@ -12,10 +12,38 @@
   "name" : "LAAL_LotsOfGarbage",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "Item" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,manager) {
+
+for (var i=0; i<100; i++) {
+	var attr = manager.getAttributeHome().getAttributeByID("Garbage-"+i);
+	logger.info(attr.getTitle());
+}
+
+}
