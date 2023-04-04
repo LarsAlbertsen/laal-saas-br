04b330b9b104cea0690a032a85dfac7ba6f6be90
Lars Albertsen <laal@stibosystems.com>
2023-04-04 07:58:13 -0400

    Committed from STEP 2023-04-04T11:58Z

--- BusinessRule_LAAL_PrintActivity.js
+++ BusinessRule_LAAL_PrintActivity.js
@@ -13,9 +13,48 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+
+var curDir = new java.io.File("./diag/activity/2023-04");
+logger.info("curDir="+curDir.getAbsolutePath().toString());
+
+var ff = getFiles(curDir,null);
+logger.info("ff="+ff);
+
+var activityFiles = getFiles(curDir,"log");
+logger.info("logFiles="+activityFiles);
+
+
+
+function getFiles(pDir, pSuffix) {
+	var rv = new java.util.ArrayList();
+	var files = pDir.listFiles();
+	for (var i=0; i<files.length; i++) {
+		if (pSuffix==null || files[i].getName().endsWith(pSuffix)) {
+			rv.add(files[i]);
+		}
+	}
+	return rv;
+}
+
+}
