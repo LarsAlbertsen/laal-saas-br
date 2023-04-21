79d2166524f91a2873471e3979e0faa12fb439f6
Lars Albertsen <laal@stibosystems.com>
2023-04-21 08:41:12 -0400

    Committed from STEP 2023-04-21T12:41Z

--- BusinessRule_CreateTestAssetsSubFolders.js
+++ BusinessRule_CreateTestAssetsSubFolders.js
@@ -12,10 +12,36 @@
   "name" : "CreateTestAssetsSubFolders",
   "description" : null,
   "scope" : "Global",
-  "validObjectTypes" : [ ],
+  "validObjectTypes" : [ "TestAssets" ],
   "allObjectTypesValid" : false,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ClassificationBindContract",
+    "alias" : "rootNode",
+    "parameterClass" : "com.stibo.core.domain.impl.FrontClassificationImpl",
+    "value" : "TestAssets",
+    "description" : null
+  }, {
+    "contract" : "ObjectTypeBindContract",
+    "alias" : "objType",
+    "parameterClass" : "com.stibo.core.domain.impl.ObjectTypeImpl",
+    "value" : "TestAssetsSubFolder",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (rootNode,objType) {
+for (var i=0; i<10; i++) {
+	rootNode.createClassification("",objType.getID());
+}
+
+}
