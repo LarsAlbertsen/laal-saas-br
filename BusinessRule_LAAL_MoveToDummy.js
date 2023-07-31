441dd74a0ef704e728ffeeca253cb6e9bf589bc1
Lars Albertsen <laal@stibosystems.com>
2023-07-31 11:32:15 -0400

    Committed from STEP 2023-07-31T15:32Z

---
+++ BusinessRule_LAAL_MoveToDummy.js
@@ -0,0 +1,44 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LAAL_MoveToDummy",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "LAAL_MoveToDummy",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Product user-type root" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ProductBindContract",
+    "alias" : "newParent",
+    "parameterClass" : "com.stibo.core.domain.impl.FrontProductImpl",
+    "value" : "Dummy",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,newParent) {
+node.setParent(newParent);
+}
