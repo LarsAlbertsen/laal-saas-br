4c0a896ffee6aab9ac9c10c42aecd5138e148f16
Lars Albertsen <laal@stibosystems.com>
2023-09-01 14:32:16 -0400

    Committed from STEP 2023-09-01T18:32Z

---
+++ BusinessRule_LAAL_MyAction.js
@@ -0,0 +1,38 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LAAL_MyAction",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "LAAL_MyAction",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "TestItem" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+node.delete();
+}
