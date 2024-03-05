371e3ec77f6af6fb632628184412ae235c460d19
Lars Albertsen <laal@stibosystems.com>
2024-03-05 21:48:10 +0800

    Committed from STEP 2024-03-05T13:48Z

--- BusinessRule_getPartObjectsToApproveByUserID.js
+++ BusinessRule_getPartObjectsToApproveByUserID.js
@@ -13,9 +13,97 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "curNode",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (curNode,logger) {
+
+var result = getPartObjectsToApproveByUserID(curNode.getManager(), curNode, logger, "LAAL");
+logger.info("result="+result);
+
+/**
+ * 
+ * @param {Manager} step
+ * @param {WorkspaceAwareRevisableNode} node
+ * @param {Logger} logger
+ * @param {String} userID
+ * @returns {HashSet} The Part Objects to Approve for the user
+ */
+function getPartObjectsToApproveByUserID(step, node, logger, userID)
+{
+    var setNonApprovedObjects = node.getNonApprovedObjects();
+    var setPartobjectsToApprove = new java.util.HashSet();
+    var getEditRevision = null;
+    try
+    {
+        getEditRevision = node.getClass().getSuperclass().getSuperclass().getSuperclass().getSuperclass().getDeclaredMethod("getEditRevision", com.stibo.core.domain.partobject.PartObject);
+    }
+    catch(e)
+    {
+        logger.warning("Could not found Method getEditRevision(). "+e.toString());
+        return setPartobjectsToApprove;
+    }
+    for(var it = setNonApprovedObjects.iterator(); it.hasNext();)
+    {
+        var partObject = it.next();
+        
+        if(partObject instanceof com.stibo.core.domain.partobject.AttributeLinkPartObject ||
+           partObject instanceof  com.stibo.core.domain.partobject.datacontainertypelink.DataContainerTypeLinkPartObject ||
+           partObject instanceof com.stibo.core.domain.partobject.TablePartObject || 
+           partObject instanceof com.stibo.core.domain.partobject.TableTextsPartObject)
+        {
+            continue;
+        }
+        /*var getEditRevision = getMethodByName(node.getClass().getSuperclass().getSuperclass().getSuperclass().getSuperclass(), "getEditRevision", logger);
+        if(getEditRevision == null)
+        {
+            logger.warning("Could not found Method getEditRevision(). ");
+            break;
+        }*/
+        
+        var editRevision = null;
+        try
+        {
+            editRevision = getEditRevision.invoke(node, partObject);
+        }
+        catch(e)
+        {
+            logger.warning("Could not invoke Method getEditRevision(). "+e.toString());
+        }
+        if(editRevision)
+        {
+            var revisionUserID = editRevision.getUserID();
+            if(revisionUserID+"" == userID+"")
+            {
+                //logger.info("PartObject "+partObject.toString()+" was edited by user "+userID);
+                setPartobjectsToApprove.add(partObject);
+            }
+        }
+        
+    }
+    return setPartobjectsToApprove;
+} 
+}
