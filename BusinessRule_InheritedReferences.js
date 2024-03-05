ff737378833d73ee1cc1c096d7adb4dde1a98e36
Lars Albertsen <laal@stibosystems.com>
2024-03-06 05:06:10 +0800

    Committed from STEP 2024-03-05T21:06Z

--- BusinessRule_InheritedReferences.js
+++ BusinessRule_InheritedReferences.js
@@ -13,9 +13,35 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ReferenceTypeBindContract",
+    "alias" : "refType",
+    "parameterClass" : "com.stibo.core.domain.impl.ReferenceTypeImpl",
+    "value" : "Generic Asset",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,refType) {
+
+var myRefs = node.getReferences(refType);
+
+logger.info("myRefs="+myRefs);
+}
