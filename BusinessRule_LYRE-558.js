810e4e13d82d3d7e8506c9013e9647c86a876ee5
Lars Albertsen <laal@stibosystems.com>
2024-03-07 23:31:11 +0800

    Committed from STEP 2024-03-07T15:31Z

---
+++ BusinessRule_LYRE-558.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LYRE-558",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "LYRE-558",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
