c6778e471c459d0bc0936e89440fe94aa520c77a
Lars Albertsen <laal@stibosystems.com>
2024-03-07 23:38:10 +0800

    Committed from STEP 2024-03-07T15:38Z

--- BusinessRule_LYRE-558.js
+++ BusinessRule_LYRE-558.js
@@ -13,9 +13,56 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "QueryHomeBindContract",
+    "alias" : "queryHome",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "AttributeBindContract",
+    "alias" : "a1",
+    "parameterClass" : "com.stibo.core.domain.impl.AttributeImpl",
+    "value" : "a1",
+    "description" : null
+  }, {
+    "contract" : "AttributeBindContract",
+    "alias" : "a2",
+    "parameterClass" : "com.stibo.core.domain.impl.AttributeImpl",
+    "value" : "a2",
+    "description" : null
+  }, {
+    "contract" : "AttributeBindContract",
+    "alias" : "a3",
+    "parameterClass" : "com.stibo.core.domain.impl.AttributeImpl",
+    "value" : "a3",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager,queryHome,a1,a2,a3) {
+var c = com.stibo.query.condition.Conditions;
+
+var conditions = c.valueOf(a1).eq("1")
+     .or(c.valueOf(a2).eq("2"))
+     .or(c.valueOf(a3).eq("3"));
+
+
+}
