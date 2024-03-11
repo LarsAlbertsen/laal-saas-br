6f7d2e72c37eddaedf89790f1387ef2fb6eea181
Lars Albertsen <laal@stibosystems.com>
2024-03-12 03:54:10 +0800

    Committed from STEP 2024-03-11T19:54Z

--- BusinessRule_TestName.js
+++ BusinessRule_TestName.js
@@ -13,9 +13,30 @@
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
-  "allObjectTypesValid" : false,
+  "allObjectTypesValid" : true,
   "runPrivileged" : false,
   "onApprove" : "Never",
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "product",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (product) {
+
+product.getName();
+
+logger.info("Name "+name);
+
+}
